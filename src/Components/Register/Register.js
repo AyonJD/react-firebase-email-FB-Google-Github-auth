07a92fae04f26374be08e13519a07d77e7b6edcb
Ayon <ayonjodder177@gmail.com>
2022-04-08 17:36:19 +0600

    Sign In with google done

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import React, { useState } from 'react';
+import auth from '../../firebase.init';
 import GoogleLogo from '../../Images/google.svg'
+import GithubLogo from '../../Images/github.svg'
+import FacebookLogo from '../../Images/facebook.svg'
+
+const googleProvider = new GoogleAuthProvider();
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState('')
+    const [error, setError] = useState('')
+    //Email Field
+    const getEmail = event => {
+        setEmail(event.target.value);
+    }
+    //Password Field
+    const getPassword = event => {
+        setPassword(event.target.value)
+    }
+    //Submit field
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        
+        //Creating user
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                setUser(user)
+                console.log(user);
+            }).catch(error => {
+                setError(error.message)
+                console.log(error.message);
+            })
+        
+    }
+    //Creating user with google signin
+    const handleGoolgeSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user;
+                setUser(user)
+                console.log(user);
+            }).catch(error => {
+                setError(error.message)
+                console.log(error.message);
+        })
+    }
     return (
         <div className='auth-form-container '>
             <div className='auth-form'>
                 <h1>Sign Up</h1>
-                <form>
+                <form onSubmit={handleFormSubmit}>
                     <div className='input-field'>
                         <label htmlFor='email'>Email</label>
                         <div className='input-wrapper'>
                             <input
+                                onBlur={getEmail}
                                 type='email'
                                 name='email'
                                 id='email'
@@ -21,6 +68,7 @@ const Register = () => {
                         <label htmlFor='password'>Password</label>
                         <div className='input-wrapper'>
                             <input
+                                onBlur={getPassword}
                                 type='password'
                                 name='password'
                                 id='password'
@@ -40,10 +88,15 @@ const Register = () => {
                     <p>or</p>
                     <div className='line-right' />
                 </div>
-                <div className='input-wrapper'>
-                    <button className='google-auth'>
+                <div className='provider-parent'>
+                    <button onClick={handleGoolgeSignIn} className='google-auth'>
                         <img src={GoogleLogo} alt='' />
-                        <p> Continue with Google </p>
+                    </button>
+                    <button className='google-auth'>
+                        <img className='github' src={GithubLogo} alt='' />
+                    </button>
+                    <button className='google-auth'>
+                        <img className='facebook' src={FacebookLogo} alt='' />
                     </button>
                 </div>
             </div>
